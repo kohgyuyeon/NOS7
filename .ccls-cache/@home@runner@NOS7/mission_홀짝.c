d158e12962ecb44dd8738d63bb88c946a54adf39
kohgyuyeon <128569882+kohgyuyeon@users.noreply.github.com>
2023-05-16 06:25:02 +0000

    조건문 끝

--- .ccls-cache/@home@runner@NOS7/mission_홀짝.c
+++ .ccls-cache/@home@runner@NOS7/mission_홀짝.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-main(){
-  int a;
-  scanf("%d", &a);
-  if(a%2==0){
-    printf("짝수입니다.");
-  }
-  else{
-    printf("홀수입니다.");
-  }
-}
+// #include <stdio.h>
+// main(){
+//   int a;
+//   scanf("%d", &a);
+//   if(a%2==0){
+//     printf("짝수입니다.");
+//   }
+//   else{
+//     printf("홀수입니다.");
+//   }
+// }
---
+++ mission_myscore.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("A+:x>94 | A:x>89 | B+:x>84 | B:x>79 | C+:x>75 | C:x>69 | D+:x>65 | D:x>59\n");
+//   scanf("%d", &x);
+
+//   if(x>94)
+//     printf("A+");
+//   else if(x>89)
+//     printf("A");
+//   else if(x>84)
+//     printf("B+");
+//   else if(x>79)
+//     printf("B");
+//   else if(x>75)
+//     printf("C+");
+//   else if(x>69)
+//     printf("C");
+//   else if(x>65)
+//     printf("D+");
+//   else if(x>59)
+//     printf("D");
+//   else
+//     printf("F");
+//   printf("채점 끝!");
+// }
